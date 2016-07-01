b9fb95a8c05206faa312689ea47eb948834ce13f
Leo Caminha <leoncio.sobreira@gmail.com>
2016-06-30 22:13:01 -0300

    First!

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,27 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
+//= require jquery/dist/jquery.min
+//= require bootstrap/dist/js/bootstrap.min
+//= require flot/jquery.flot
+//= require flot/jquery.flot.resize
+//= require flot-orderBars/js/jquery.flot.orderBars
+//= require flot.curvedlines/curvedLines
+//= require jquery.sparkline.min
+//= require jquery.easy-pie-chart/dist/jquery.easypiechart.min
+//= require flot-orderBars/js/jquery.flot.orderBars
+//= require malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min
+//= require functions
+//= require charts
