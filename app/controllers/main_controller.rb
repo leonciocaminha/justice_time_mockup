36de3d4acdd4a5ccee2635198bb5a0e3fb0e2994
Leoncio Caminha <leoncio.sobreira@gmail.com>
2016-07-01 17:31:55 -0300

    Some things done

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,8 @@
 class MainController < ApplicationController
   def index
   end
+
+  def phase_3
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'main#index'
+  get "main/phase_3"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
